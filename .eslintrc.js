a4bceefc1456ee19b17a5bd4307272eed23cbba5
captain-something <benoau@pprotonmail.com>
2026-08-19 13:29:54 -0600

    add prettier/editorconfig/line length limits

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,9 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint/eslint-plugin', 
-    'check-file',
-    'folders',
-    'sort-imports-es6-autofix'
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'check-file', 'folders', 'sort-imports-es6-autofix'],
   extends: [
-    'airbnb-base', 
+    'airbnb-base',
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -25,23 +20,28 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js', 'jest.*', 'dist', 'node_modules', 'api-schema.ts'],
   rules: {
     // single quotes
-    '@typescript-eslint/quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    '@typescript-eslint/quotes': ['error', 'single', { allowTemplateLiterals: true }],
     'folders/match-regex': [2, /^[a-z0-9-]+$/, '/src'],
-    'check-file/filename-naming-convention':[
-       'error',
-       {
-          '*.{js,ts,json}':'KEBAB_CASE'
-       },
+    'check-file/filename-naming-convention': [
+      'error',
+      {
+        '*.{js,ts,json}': 'KEBAB_CASE',
+      },
     ],
     // module import/export sorting
     'import/order': 'off',
     'import/no-cycle': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
-    'sort-imports-es6-autofix/sort-imports-es6': [2, {
-      ignoreCase: false,
-      ignoreMemberSort: false,
-      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
-    }],
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    // line length
+    'max-len': ['error', { code: 120 }],
   },
 };
