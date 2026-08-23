b6948065654922c199c9405db03bc44fb4287a19
captain-something <benoau@pprotonmail.com>
2026-08-23 10:53:29 -0600

    consistent parameter name

--- src/api/admin/update-user-roles/update-user-roles.controller.ts
+++ src/api/admin/update-user-roles/update-user-roles.controller.ts
@@ -59,7 +59,7 @@ export class AdminUpdateUserRolesController {
     @Query() query: AdminUpdateUserRolesQueryDto,
     @Body() body: AdminUpdateUserRolesBodyDto,
   ): Promise<AdminUpdateUserRolesResponseDto> {
-    await this.updateRolesService.updateUserRoles(user.id, query.accountId, body.roles);
+    await this.updateRolesService.updateUserRoles(user.id, query.id, body.roles);
     return {
       success: true,
     };
