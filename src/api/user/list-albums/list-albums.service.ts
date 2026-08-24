559e60a64a451fffc2ab530b2256ba2fe8e5a85f
captain-something <benoau@pprotonmail.com>
2026-08-23 19:23:45 -0600

    remove redundant object

--- src/api/user/list-albums/list-albums.service.ts
+++ src/api/user/list-albums/list-albums.service.ts
@@ -7,20 +7,9 @@ export class UserListAlbumsService {
   constructor(private readonly libraryService: LibraryService) {}
 
   async listAlbums(accountId: number, query: UserListAlbumsQueryDto) {
-    const filter = {
-      filter: query.filter,
-      artist: query.artist,
-      composer: query.composer,
-      genre: query.genre,
-      year: query.year,
-      minRating: query.minRating,
-      maxRating: query.maxRating,
-      addedBefore: query.addedBefore,
-      addedAfter: query.addedAfter,
-    };
     const albums = await this.libraryService.listAlbums(
       accountId,
-      filter,
+      query,
       query.offset || 0,
       query.limit || 100_000,
       query.sortField,
