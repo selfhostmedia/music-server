978c04f2f8d8130c52d1eb5b31d4b145c983ba3b
captain-something <benoau@pprotonmail.com>
2026-08-27 09:24:03 -0600

    rebase on library service

--- src/api/synology-audiostation/artist.service.ts
+++ src/api/synology-audiostation/artist.service.ts
@@ -1,12 +1,10 @@
-import { CollatedArtistEntity, CollatedGenreAlbumEntity, GenreEntity } from 'src/database/entities';
-import { InjectModel } from '@nestjs/sequelize';
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Op } from 'sequelize';
-import { Sequelize } from 'sequelize-typescript';
+import { Injectable } from '@nestjs/common';
+import { LibraryArtistDto } from 'src/library/dtos/library.artist.dto';
+import { LibraryService } from 'src/library/library.service';
 import { SynologyArtistDataDto, SynologyArtistDto } from './dtos';
-import { normalizeString, replaceDoubleQuotes } from 'src/utils/strings';
+import { replaceDoubleQuotes } from 'src/utils/strings';
 
-function personToRow(person: CollatedArtistEntity): SynologyArtistDto {
+function personToRow(person: LibraryArtistDto): SynologyArtistDto {
   return {
     additional: {
       artist_rating: {
@@ -17,48 +15,16 @@ function personToRow(person: CollatedArtistEntity): SynologyArtistDto {
     name: replaceDoubleQuotes(person.name),
   };
 }
-
-function albumToRow(album: CollatedGenreAlbumEntity) {
-  return {
-    additional: {
-      artist_rating: {
-        rating: 0,
-      },
-    },
-    id: `album_${album.id}`,
-    name: replaceDoubleQuotes(album.artist),
-  };
-}
-
 @Injectable()
 export class SynologyArtistService {
-  constructor(
-    @InjectModel(CollatedArtistEntity)
-    private readonly collatedArtistEntity: typeof CollatedArtistEntity,
-    @InjectModel(CollatedGenreAlbumEntity)
-    private readonly collatedGenreAlbumEntity: typeof CollatedGenreAlbumEntity,
-    @InjectModel(GenreEntity)
-    private readonly genreEntity: typeof GenreEntity,
-  ) {}
+  constructor(private readonly libraryService: LibraryService) {}
 
   async listArtists(accountId: number, offset: number, limit: number): Promise<SynologyArtistDataDto> {
-    const artists = await this.collatedArtistEntity.findAll({
-      where: {
-        accountId,
-      },
-      offset,
-      limit: limit || 1000,
-      order: [['name', 'ASC']],
-    });
-    const total = await this.collatedArtistEntity.count({
-      where: {
-        accountId,
-      },
-    });
+    const artists = await this.libraryService.listAlbumArtists(accountId, {}, offset, limit);
     return {
-      artists: artists.map(personToRow),
+      artists: artists.items.map(personToRow),
       offset,
-      total,
+      total: artists.total,
     };
   }
 
@@ -68,43 +34,18 @@ export class SynologyArtistService {
     offset: number,
     limit: number,
   ): Promise<SynologyArtistDataDto> {
-    const genres = await this.genreEntity.findAll({
-      attributes: ['id'],
-      where: {
-        accountId,
-        nameNormalized: {
-          [Op.in]: genreName.split('/').map(normalizeString),
-        },
+    const artists = await this.libraryService.listAlbumArtists(
+      accountId,
+      {
+        genre: genreName.split('/'),
       },
-    });
-    if (!genres?.length) {
-      throw new NotFoundException(`Genre not found: ${genreName}`);
-    }
-    const artists = await this.collatedGenreAlbumEntity.findAll({
-      where: {
-        accountId,
-        genreId: {
-          [Op.in]: genres.map((genre) => genre.id),
-        },
-      },
-      group: ['CollatedGenreAlbumEntity.artist'],
-      order: [['title', 'ASC']],
       offset,
-      limit: limit || 100000,
-    });
-    const total = await this.collatedGenreAlbumEntity.count({
-      attributes: [[Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('title'))), 'count']],
-      where: {
-        accountId,
-        genreId: {
-          [Op.in]: genres.map((genre) => genre.id),
-        },
-      },
-    });
+      limit,
+    );
     return {
-      artists: artists.map(albumToRow),
+      artists: artists.items.map(personToRow),
       offset,
-      total,
+      total: artists.total,
     };
   }
 }
